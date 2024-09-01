2d6caffefb5df53e7c2ff4c7acfbb5afce479bdd
Назаров Михаил <85027066+movetron@users.noreply.github.com>
2024-09-01 13:53:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    const isNegative = n < 0;
+  const reversedNumber = parseInt(Math.abs(n).toString().split('').reverse().join(''), 10);
+  return isNegative ? Math.abs(reversedNumber) : reversedNumber;
 }
